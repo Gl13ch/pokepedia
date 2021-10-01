23c0e9b5022233c5106eb448be7343de9d25e349
Kaylee Spindler <gl13ch@Kaylees-MacBook-Air.local>
2021-10-01 15:03:19 -0500

    can click on read more and read more about that pokemon

--- app.js
+++ app.js
@@ -1,54 +1,54 @@
-//MINIMUM REQUIREMENTS
+$(() => {
 
-//HTML, CSS, JavaScript, jQuery
-//Hosted
-//Commit, Commit, Commit
-//A README.md
-
-//Use ajax
-//responsive design - must be compatible on pc and mobile
-//Have at least one:
-//Carousel, modal, sticky nav, tooltips, etc.
-
-
-const showPokemon = (data) => {
-  for (let i = 0; i < 20; i++) {
-
+const showPokemonGen = (data) => {
+  for (let i = 0; i < 5; i++) {
     const $pokemon = $('<div>').addClass('pokemon').attr('id', i).text(data.pokemon_species[i].name).appendTo($('.poke_container'))
 
-    const $newButton = $('<button>').text('Read More').addClass('more-info').appendTo($pokemon)
+    const $newButton = $('<button>').val(data.pokemon_species[i].name).text('Read More').addClass('read-more').appendTo($pokemon)
   }
-  // const $btn = $('.more-info').on('click', event => {
-  //
-  //   const $id = $(event.target).parent().attr('id')
-  //
-  //   let $pokedata = $('<div>').text(data.pokemon_species[$id].name).addClass('more-info')
-    // $('.more-info').remove()
 
-    // if( $newDiv ){
-    //   $newDiv.appendTo($(event.currentTarget).parent())
-    //   $newDiv = null
-    // }else{
-    //   $newDiv = $('.more-info').detach()
-    // }
-  // })
-}
+  $('.read-more').on('click', event => {
+    const name = $(event.target).val()
+    $('.more-info').remove()
+
+     $.ajax({
+        url:`https://pokeapi.co/api/v2/pokemon/${name}?limit=20&offset=20`
+     }).then(
+         (data)=>{
+           // console.log(data);
+           let $pokeData = $('<div>').addClass('more-info').text(`${data.name} is a ${data.types[0].type.name} type.`)
+
+           if( $pokeData ){
+             $pokeData.appendTo($(event.currentTarget).parent())
+             $pokeData = null
+           }else{
+             $pokeData = $('.more-info').detach()
+           }
+
+         },
+         ()=>{
+           console.log('bad request');
+       }
+     )
+   })
+  }
+
 //pokemon api
-$(() => {
-  $('button').on('click', (event) => {
+
+  const $btn1 = $('.gen').on('click', (event) => {
     event.preventDefault()
       const generation = $(event.currentTarget).val()
       $('.pokemon').remove()
   $.ajax({
-     url:`https://pokeapi.co/api/v2/generation/${generation}/?limit=20&offset=20`
+     url:`https://pokeapi.co/api/v2/generation/${generation}/?limit=5&offset=5`
   }).then(
       (data)=>{
-         console.log(data);
-         showPokemon(data)
+         // console.log(data);
+         showPokemonGen(data)
       },
       ()=>{
         console.log('bad request');
-    }
-  );
-})
+      }
+    );
+  })
 })
