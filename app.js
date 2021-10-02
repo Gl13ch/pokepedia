6a16f93848e4e8ee2064506afc4abf8f4177e8b2
Kaylee Spindler <gl13ch@Kaylees-MacBook-Air.local>
2021-10-02 01:57:41 -0500

    got all pictures working

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 $(() => {
 
 const showPokemonGen = (data) => {
-  for (let i = 0; i < 5; i++) {
-    const $pokemon = $('<div>').addClass('pokemon').attr('id', i).text(data.pokemon_species[i].name).appendTo($('.poke_container'))
+  for (let i = 0; i < Object.keys(data.pokemon_species).length; i++) {
+    // console.log(Object.keys(data).length);
+    const $pokemon = $('<div>').addClass('pokemon').attr('id', i) .appendTo($('.poke_container'))
 
-    const $newButton = $('<button>').val(data.pokemon_species[i].name).text('Read More').addClass('read-more').appendTo($pokemon)
+    const $newButton = $('<button>').val(data.pokemon_species[i].name).text(data.pokemon_species[i].name).addClass('read-more').appendTo($pokemon)
   }
 
   $('.read-more').on('click', event => {
     const name = $(event.target).val()
     $('.more-info').remove()
+    $('.img_container').remove()
 
      $.ajax({
         url:`https://pokeapi.co/api/v2/pokemon/${name}?limit=20&offset=20`
@@ -18,11 +20,17 @@ const showPokemonGen = (data) => {
            // console.log(data);
            let $pokeData = $('<div>').addClass('more-info').text(`${data.name} is a ${data.types[0].type.name} type.`)
 
+           const $imgContainer = $('<div>').addClass('img_container')
+
+           let $pokeImg = $('<img>').attr('src', data.sprites.other["official-artwork"].front_default).addClass('poke_img').appendTo($imgContainer)
+
            if( $pokeData ){
              $pokeData.appendTo($(event.currentTarget).parent())
+             $imgContainer.appendTo($(event.currentTarget).parent())
              $pokeData = null
            }else{
              $pokeData = $('.more-info').detach()
+             $imgContainer = $('.img_container').detach()
            }
 
          },
@@ -39,6 +47,9 @@ const showPokemonGen = (data) => {
     event.preventDefault()
       const generation = $(event.currentTarget).val()
       $('.pokemon').remove()
+      $('.poke-search').remove()
+      $('.poke_search_img').remove()
+
   $.ajax({
      url:`https://pokeapi.co/api/v2/generation/${generation}/?limit=5&offset=5`
   }).then(
@@ -55,16 +66,23 @@ const showPokemonGen = (data) => {
   $('form').on('submit', event => {
     event.preventDefault()
     const userInput = $('#search-poke').val()
+
     $('.poke-search').remove()
+    $('.pokemon').remove()
+    $('.img_search_container').remove()
 
      $.ajax({
-        url:`https://pokeapi.co/api/v2/pokemon/${userInput}?limit=20&offset=20`
+        url:`https://pokeapi.co/api/v2/pokemon/${userInput}?limit=200&offset=200`
      }).then(
          (data)=>{
 
            console.log(data);
 
            const $search = $('<div>').addClass('poke-search').text(`${data.name} is a ${data.types[0].type.name} type.`).appendTo($('.poke_container'))
+
+           const $imgSearchContainer = $('<div>').addClass('img_search_container').appendTo($('.poke_container'))
+
+           let $pokeSearchImg = $('<img>').attr('src', data.sprites.other["official-artwork"].front_default).addClass('poke_search_img').appendTo($imgSearchContainer)
         },
         ()=>{
           console.log('bad request');
@@ -72,3 +90,7 @@ const showPokemonGen = (data) => {
     );
   })
 })
+
+// "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png"
+//
+// data.sprites.other['official-artwork'].front_default
